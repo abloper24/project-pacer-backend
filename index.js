64116c361260f31d248546f1ec54cff6a33acf5e
abloper24 <ablobelperez@gmail.com>
2024-03-20 06:31:21 -0700

    added basic structure

---
+++ index.js
@@ -0,0 +1,22 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors'); 
+// const PORT = 8080;
+
+// const videosRoutes = require('./routes/videos') 
+
+// app.use(cors()); 
+// app.use(express.json()); 
+// app.use('/videos', videosRoutes) 
+
+// app.use((req, res, next)=>{
+//     console.log('Incoming Request!')
+//     console.log(req.method)
+//     next();
+// });
+
+// app.use(express.static('public'));
+
+// app.listen(PORT, () => {
+//     console.log(`We are live on port ${PORT}`)
+// })
